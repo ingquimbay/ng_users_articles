eb8d949a1b96efdc0ebb7644035e9b0a5a1b8839
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-14 18:36:42 -0600

    Implemented a user service

---
+++ src/app/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from "./user";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+
+  private url: string = 'http://localhost:3000/api/user';
+  constructor(private http: HttpClient) { }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url);
+  }
+}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from "../users.service";
+import { User } from "../user";
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
+
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.usersService.getUsers().subscribe((response) => {
+      this.users = response, console.log(this.users);
+    });
   }
 
 }

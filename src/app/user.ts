86e68cc092c8bdcfdb9f1dcd73c73ace3809792d
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-14 18:21:52 -0600

    added user schema

---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor() {
+    }
+
+    _id: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    username: string;
+    admin: boolean;
+}

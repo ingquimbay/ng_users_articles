6d1343976ad814c193f16ea81aa7112f5bd88892
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-14 19:16:47 -0600

    Implemented a user view

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -14,8 +14,14 @@ const httpOptions = {
 export class UsersService {
 
   private url: string = 'http://localhost:3000/api/user';
+
   constructor(private http: HttpClient) { }
+
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.url);
+  }
+
+  getUser(id: string): Observable<User>{
+    return this.http.get<User>(`${this.url}/${id}`);
   }
 }

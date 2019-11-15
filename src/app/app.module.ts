a922db6e9cbe2a1199f9c612031d72f4ce9cb1ff
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-14 19:27:39 -0600

    Imported FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from "@angular/common/http";
 import { UserViewComponent } from './user-view/user-view.component';
 import { UserCreateComponent } from './user-create/user-create.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { UserCreateComponent } from './user-create/user-create.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

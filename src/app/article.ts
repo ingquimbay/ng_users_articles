e374d4aa325240e00aac3a07d36c47c49a7035ce
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-14 20:48:26 -0600

    created article class

---
+++ src/app/article.ts
@@ -0,0 +1,14 @@
+export class Article {
+    constructor() {
+    }
+
+    _id: string;
+    title: string;
+    slug: string;
+    description: string;
+    keywords: string;
+    body: string;
+    published: Date;
+    created: Date;
+    modified: Date;
+}

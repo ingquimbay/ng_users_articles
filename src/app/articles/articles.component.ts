6bab5f67498960cf60a6e858fa8cd124fb7d9fe2
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-19 19:56:41 -0600

    article routing

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from "../article.service";
+import { Article } from "../article";
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
+
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  articles: Article[];
+
+  constructor(private articlesService: ArticleService) { }
 
   ngOnInit() {
+    this.getArticles();
+  }
+
+  getArticles() {
+    this.articlesService.getArticles().subscribe((response) => {
+      this.articles = response, console.log(this.articles);
+    })
   }
 
 }

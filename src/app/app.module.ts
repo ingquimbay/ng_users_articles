d6a4b9af759cc6aad90fad14ea2a06ab88d2f08b
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-19 20:25:48 -0600

    added article create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FormsModule } from "@angular/forms";
 import { UserEditComponent } from './user-edit/user-edit.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleViewComponent } from './article-view/article-view.component';
+import { ArticleCreateComponent } from './article-create/article-create.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ArticleViewComponent } from './article-view/article-view.component';
     UserCreateComponent,
     UserEditComponent,
     ArticlesComponent,
-    ArticleViewComponent
+    ArticleViewComponent,
+    ArticleCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/article-create/article-create.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Router, Route } from "@angular/router";
+import { ArticleService } from "../article.service";
+import { Article } from "../article";
+
+@Component({
+  selector: 'app-article-create',
+  templateUrl: './article-create.component.html',
+  styleUrls: ['./article-create.component.scss']
+})
+export class ArticleCreateComponent implements OnInit {
+
+  article = new Article();
+  errors: Array<any> = [];
+  errorMessage: string;
+
+  constructor(
+    private articleService: ArticleService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  response(response): void {
+    if (response.success === false) {
+      this.errors = response.error.errors;
+      this.errorMessage = response.error.message;
+    }
+    if (response.success === true) {
+      this.router.navigate(['/articles/view/', response.article._id]);
+    }
+  }
+
+  onSubmit(): void {
+    this.articleService.createArticle(this.article).subscribe((response) => { this.response(response) });
+  }
+
+}
